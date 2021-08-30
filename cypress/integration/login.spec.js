e5e3b99944ed595453ebed36f3ebb0575fa6e70e
mariellemeira <mariellemeira901@gmail.com>
2021-08-30 17:10:30 -0300

    Create e2e tests with cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,13 @@
+it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+  
+    cy.login(
+      Cypress.env('USER_EMAIL'),
+      Cypress.env('USER_PASSWORD'),
+      { cacheSession: false }
+    )
+  
+    cy.wait('@getNotes')
+  })
+  
+  
---
+++ cypress/support/index.js
@@ -0,0 +1,4 @@
+import 'cypress-mailosaur'
+import 'cypress-file-upload'
+import 'cypress-iframe'
+import './commands'
